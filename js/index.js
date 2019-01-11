34a067341f965bec9400a1ae45d3302e3c413b2b
jodyneckles <jody@technative.tv>
2019-01-11 15:42:28 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,34 @@
-// using this file is optional
-// you can also load the code snippets in using your browser's console
+// OBJECTIVES
+// Demonstrate listening to events on a DOM node with addEventListener()
+// Demonstrate triggering events listeners on DOM nodes
+// Demonstrate preventing the default behavior for DOM nodes
+// Explain the difference between bubbling and capturing events
+// Demonstrate stopping propagated behaviors with stopPropagation()
+
+
+
+// log key events. Use preventDefault() to log the letter g
+const input = document.querySelector('input')
+
+input.addEventListener('keydown', function(e) {
+  if (e.key === "g") {
+    return e.preventDefault()
+  } else {
+    console.log(e.key)
+  }
+});
+
+
+// create a bubble event only on the clicked div. Restrict using stopPropagation()
+const divs = document.querySelectorAll('div');
+
+function bubble(e) {
+  // stop! that! propagation!
+  e.stopPropagation();
+
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
